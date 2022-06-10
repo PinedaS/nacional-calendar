c38bf7f517616ef0521901b43851a0f2cb24d221
Sebastián Pineda <spineda0424@gmail.com>
2022-06-09 20:40:42 -0500

    Corrección de la lógica debido a cambios en la página del equipo

--- src/scraper.py
+++ src/scraper.py
@@ -4,44 +4,27 @@ import lxml.html as html
 
 HOME_URL = "https://www.atlnacional.com.co/"
 
-XPATH_LINK_TO_FOOTBALL_MATCH = "//div[@class='contenedor-partido']/ul/li[2]/a/@href"
-XPATH_DATE_FOOTBALL_MATCH = "//div[@class='info-partido-actual inline-elements']/p[3]"
-XPATH_TEAM1 = "//div[@class='equipo-left']/div[1]/p[1]/text()"
-XPATH_TEAM2 = "//div[@class='equipo-right']/div[1]/p[1]/text()"
+XPATH_DATE_AND_TIME_FOOTBALL_MATCH = "//div[@class='jet-listing-dynamic-repeater__item'][1]/div/div/div[2]/span"
+XPATH_TEAM1 = "//div[@class='jet-listing-dynamic-repeater__item'][1]/div/div/div[1]/div[1]/h3"
+XPATH_TEAM2 = "//div[@class='jet-listing-dynamic-repeater__item'][1]/div/div/div[1]/div[2]/h3"
 
 
-def parse_home():
+def parse_info():
     try:
         response = requests.get(HOME_URL)
-
-        if response.status_code == 200:
-            home = response.content.decode("utf-8")
-            parsed = html.fromstring(home)
-            link_to_football_match = parsed.xpath(
-                XPATH_LINK_TO_FOOTBALL_MATCH)[0]
-
-            return link_to_football_match
-        else:
-            raise ValueError("Error: " + response.status_code)
-
-    except ValueError as ve:
-        print(ve)
-
-
-def parse_info(link_to_football_match):
-    try:
-        response = requests.get(link_to_football_match)
 
         if response.status_code == 200:
             info = response.content.decode("utf-8")
             parsed = html.fromstring(info)
 
             date_football_match = parsed.xpath(
-                XPATH_DATE_FOOTBALL_MATCH)[0].text
-            team1 = parsed.xpath(XPATH_TEAM1)[0]
-            team2 = parsed.xpath(XPATH_TEAM2)[0]
+                XPATH_DATE_AND_TIME_FOOTBALL_MATCH)[0].text
+            time_football_match = parsed.xpath(
+                XPATH_DATE_AND_TIME_FOOTBALL_MATCH)[1].text
+            team1 = parsed.xpath(XPATH_TEAM1)[0].text
+            team2 = parsed.xpath(XPATH_TEAM2)[0].text
 
-            return [date_football_match, team1, team2]
+            return [date_football_match, time_football_match, team1, team2]
 
         else:
             raise ValueError("Error: " + response.status_code)
@@ -51,9 +34,7 @@ def parse_info(link_to_football_match):
 
 
 def run():
-    link_to_football_match = parse_home()
-
-    return parse_info(link_to_football_match)
+    return parse_info()
 
 
 if __name__ == '__main__':
